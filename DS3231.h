15b72fee151646d0aca80255458a2f8100afce3f
KonstantinDM <36813192+KonstantinDM@users.noreply.github.com>
2019-03-24 18:55:36 +0500

    Update DS3231.h

--- DS3231.h
+++ DS3231.h
@@ -10,16 +10,16 @@ class DS3231RTC : public I2CDevice
 {
 	
 public:
-	
-	DS3231RTC(I2C_HandleTypeDef* in_i2cHandle, u8 in_u8Address) : I2CDevice(in_i2cHandle, in_u8Address){};
-	void Update();
 
-	void SetTime(u8 in_Hour, u8 in_Min, u8 in_Sec);
+   DS3231RTC(I2C_HandleTypeDef* in_i2cHandle, u8 in_u8Address) : I2CDevice(in_i2cHandle, in_u8Address){};
+   void Update();
+
+   void SetTime(u8 in_Hour, u8 in_Min, u8 in_Sec);
    void SetDate(u8 in_Day, u8 in_Month, u8 in_Year);		
 		
-	void GetTimeChars(char* out_cStr);
-	void GetDateChars(char* out_cStr);
-	void GetTemperatureChars(char* out_cStr);
+   void GetTimeChars(char* out_cStr);
+   void GetDateChars(char* out_cStr);
+   void GetTemperatureChars(char* out_cStr);
 
 protected:
 	
